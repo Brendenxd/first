26190bbf4ce11d49d1f933bd4f78118f3353101d
Brenden Buttenmuller <143776181+Brendenxd@users.noreply.github.com>
2023-09-12 09:47:49 -0400

    Add files via upload

---
+++ Quotation.java
@@ -0,0 +1,18 @@
+// Program Name: Quotation
+// Description: Print out a statement on a monitor
+// Name: Brenden Buttenmuller
+// Date: 9/11/23
+
+public class Quotation
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Life would be tragic if it was not funny.");
+        System.out.println("-Stephen Hawking");
+    }
+}
+/*
+ Life would be tragic if it was not funny.
+-Stephen Hawking
+Press any key to continue . . .
+ */
